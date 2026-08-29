fd092ac0e528ae209e08580488c1bf672806be04
marymajmr1 <maria9713mra@gmail.com>
2026-08-28 20:46:49 -0500

    Add functions to show message and view product

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
+function mostrarMensaje() {
+    alert("¡Bienvenida/o a Angel's Fashion World!");
+}
 
+function verProducto(categoria) {
+    alert("Próximamente encontrarás nuestros productos de " + categoria + " 🛍️");
+}
